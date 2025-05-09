5360631d6c448af0246354326b77904e3e6554dd
batsi <vooj@gmx.net>
2025-05-09 22:51:53 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVICE_NAME=$(basename $SCRIPT_DIR)
+
+# create sym-link to run script in deamon
+ln -s $SCRIPT_DIR/service /service/$SERVICE_NAME
+
+filename=/data/rc.local
+if [ ! -f $filename ]
+then
+    touch $filename
+    chmod 755 $filename
+    echo "#!/bin/bash" >> $filename
+    echo >> $filename
+fi
+
+grep -qxF "$SCRIPT_DIR/install.sh" $filename || echo "/bin/bash $SCRIPT_DIR/install.sh" >> $filename
