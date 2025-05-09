ec3858051986ace4b9b59efec4bdcabcbfb576d6
batsi <vooj@gmx.net>
2025-05-10 00:06:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
-# create sym-link to run script in deamon
+chmod a+x $SCRIPT_DIR/install.sh
+chmod 744 $SCRIPT_DIR/install.sh
+
+chmod a+x $SCRIPT_DIR/service/run
+chmod 755 $SCRIPT_DIR/service/run
+
 ln -s $SCRIPT_DIR/service /service/$SERVICE_NAME
 
 filename=/data/rc.local
