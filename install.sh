abcf079942367a4523ca671dc729d16f9511ce89
batsi <vooj@gmx.net>
2025-05-10 19:15:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
+RC_LOCAL_FILE=/data/rc.local
 
 chmod a+x $SCRIPT_DIR/install.sh
 chmod 744 $SCRIPT_DIR/install.sh
+
+chmod a+x $SCRIPT_DIR/uninstall.sh
+chmod 744 $SCRIPT_DIR/uninstall.sh
 
 chmod a+x $SCRIPT_DIR/service/run
 chmod 755 $SCRIPT_DIR/service/run
 
 ln -s $SCRIPT_DIR/service /service/$SERVICE_NAME
 
-filename=/data/rc.local
-if [ ! -f $filename ]
+if [ ! -f $RC_LOCAL_FILE ]
 then
-    touch $filename
-    chmod 755 $filename
-    echo "#!/bin/bash" >> $filename
-    echo >> $filename
+    touch $RC_LOCAL_FILE
+    chmod 755 $RC_LOCAL_FILE
+    echo "#!/bin/bash" >> $RC_LOCAL_FILE
+    echo >> $RC_LOCAL_FILE
 fi
 
-grep -qxF "$SCRIPT_DIR/install.sh" $filename || echo "$SCRIPT_DIR/install.sh" >> $filename
+grep -qxF "$SCRIPT_DIR/install.sh" $RC_LOCAL_FILE || echo "/bin/bash $SCRIPT_DIR/install.sh" >> $RC_LOCAL_FILE
